b0bd752fcd17ad4e773b5b8a065a3209ba03ff5b
Mohamed Ezer Bouzouraa <mohamedezer.bouzouraa@insat.ucar.tn>
2026-03-02 12:27:23 +0100

    Create reservation.sql

---
+++ 02_tables/reservation.sql
@@ -0,0 +1,24 @@
+CREATE TABLE reservation (
+    reservation_id INTEGER PRIMARY KEY,
+    batiment VARCHAR(1),
+    numero_salle VARCHAR(10),
+    enseignement_id INTEGER,
+    departement_id INTEGER,
+    enseignant_id INTEGER,
+    date_resa DATE,
+    heure_debut TIME,
+    heure_fin TIME,
+    nombre_heures INTEGER,
+    CONSTRAINT fk_reservation_salle
+        FOREIGN KEY (batiment, numero_salle)
+        REFERENCES salle(batiment, numero_salle),
+    CONSTRAINT fk_reservation_enseignement
+        FOREIGN KEY (enseignement_id)
+        REFERENCES enseignement(enseignement_id),
+    CONSTRAINT fk_reservation_departement
+        FOREIGN KEY (departement_id)
+        REFERENCES departement(departement_id),
+    CONSTRAINT fk_reservation_enseignant
+        FOREIGN KEY (enseignant_id)
+        REFERENCES enseignant(enseignant_id)
+);
