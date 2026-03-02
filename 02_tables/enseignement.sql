9281a2ed06ef5f9e7ca14e409790680c02d33d0a
Mohamed Ezer Bouzouraa <mohamedezer.bouzouraa@insat.ucar.tn>
2026-03-02 12:24:11 +0100

    Create enseignement.sql

---
+++ 02_tables/enseignement.sql
@@ -0,0 +1,9 @@
+CREATE TABLE enseignement (
+    enseignement_id INTEGER PRIMARY KEY,
+    departement_id INTEGER,
+    intitule VARCHAR(60),
+    description VARCHAR(1000),
+    CONSTRAINT fk_enseignement_departement
+        FOREIGN KEY (departement_id)
+        REFERENCES departement(departement_id)
+);
