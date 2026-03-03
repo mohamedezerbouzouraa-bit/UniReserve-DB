5f7579611c734465f11fd0f4b8ca67f249ea8fd9
Mohamed Ezer Bouzouraa <mohamedezer.bouzouraa@insat.ucar.tn>
2026-03-03 11:40:25 +0100

    Create enseignant_data.sql

---
+++ database/04_inserts/enseignant_data.sql
@@ -0,0 +1,2 @@
+INSERT INTO enseignant VALUES (1,1,'ousteith','ouahed','MCF','4185','4091','ousteith@gmail.com');
+INSERT INTO enseignant VALUES (2,1,'ousteitha','wahida','PROF',NULL,NULL,'wahida@gmail.com');
