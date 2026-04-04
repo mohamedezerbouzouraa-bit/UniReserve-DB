378943d8998689c0a26394fa61e1209ad8994334
Mohamed Ezer Bouzouraa <mohamedezer.bouzouraa@insat.ucar.tn>
2026-04-05 00:36:11 +0200

    Add initial data to enseignement table

---
+++ insertions/into_enseignement.sql
@@ -0,0 +1,3 @@
+INSERT INTO enseignement VALUES (1,1,'Bases de Données Relationnelles','Niveau Licence (L3) : Modélisation E/A et UML, Modèle relationnel, Algèbre Relationnelle, Calcul relationnel, SQL, dépendances fonctionnelles et formes normales');
+INSERT INTO enseignement VALUES (2,1,'Langage C++','Niveau Master 1');
+INSERT INTO enseignement VALUES (3,1,'Mise à Niveau Bases de Données','Niveau Master 2 - Programme Licence et Master 1 en Bases de Données');
