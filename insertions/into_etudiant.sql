e3311a33c8fce4b31fad7c678c6dd79f95386523
Mohamed Ezer Bouzouraa <mohamedezer.bouzouraa@insat.ucar.tn>
2026-04-05 00:35:08 +0200

    Add initial student records to etudiant table

---
+++ insertions/into_etudiant.sql
@@ -0,0 +1,5 @@
+INSERT INTO etudiant VALUES (1,'ben foulen', 'foulen','1979-02-18','50, Rue des alouettes','TUNIS','75021','0143567890',NULL,'foulen@gmail.com');
+INSERT INTO etudiant VALUES (2,'tounsi', 'ahmed','1980-08-23','10, Avenue des marguerites','bardo','40000','0678567801',NULL,'pat@yahoo.fr');
+INSERT INTO etudiant VALUES (3,'tounsi', 'Jamal','1978-05-12','25, Boulevard des fleurs','TUNIS','75022','0145678956','0145678956','odent@free.fr');
+INSERT INTO etudiant VALUES (4,'benmard', 'ahmed','1979-07-15','56, Boulevard des fleurs','TUNIS','75022','0678905645',NULL,'deby@hotmail.com');
+INSERT INTO etudiant VALUES (5,'foulana', 'tounsia','1979-08-15','45, Avenue des abeilles','ariana','75022',NULL,NULL,NULL);
